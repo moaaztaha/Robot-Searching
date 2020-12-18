d71302a519722dc5c7f3d952fed30d202516fb17
moaztaha <moaaztaha7@gmal.com>
2020-12-18 17:04:35 +0200

    A* working

--- main.py
+++ main.py
@@ -133,13 +133,16 @@ def algorithm(draw, grid, start , end):
         # the algo
         for neighbor in current.neighbors:
             temp_g_score = g_score[current] + 1
-            g_score[neighbor] = temp_g_score
-            f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-            if neighbor not in open_set_hash:
-                count += 1
-                open_set.put((f_score[neighbor], count, neighbor))
-                open_set_hash.add(neighbor)
-                neighbor.make_open()
+            
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
         draw()
 
         if current != start:
